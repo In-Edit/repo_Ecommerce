9fe2869ab2dd51b1a2c10b1a02fe1f541ad11e9b
unknown <diego17_91@hotmail.com>
2024-07-10 17:27:21 -0500

    Segunda entrega: Creacion del codigo funcional para la transformacion de la data

--- data_transform.py
+++ data_transform.py
@@ -1,1 +1,20 @@
 #Proyecto Final
+import pandas as pd
+
+# Ruta del archivo CSV intermedio
+archivo_csv = 'ventasEcommerce.csv'
+
+try:
+    # Leer el archivo CSV
+  data = pd.read_csv(archivo_csv)
+    
+    # Ordenar los datos por nombre
+  data_ordenada = data.sort_values(by='Country')
+   
+    # Guardar archivo CSV ordenado y filtrado
+    archivo_csv_ordenado = 'ventasEcommerce_ordenada.csv'
+    data_ordenada.to_csv(archivo_csv_ordenado, index=False, encoding='utf-8')
+    
+    print(f"Datos exportados exitosamente a {archivo_csv_ordenado}")
+except Exception as e:
+    print(f"Error al transformar los datos: {e}")
